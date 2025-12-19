8659d2d0ee0873241eb3e6833dafbd19e917e504
DuvanMotavita <duvanfel1@yahoo.com.co>
2025-12-19 16:16:51 -0500

    Added new iterator and mediator patterns

--- 03-comportamiento/03-iterator.ts
+++ 03-comportamiento/03-iterator.ts
@@ -8,3 +8,87 @@
  *
  * https://refactoring.guru/es/design-patterns/iterator
  */
+
+interface Iterator<T> {
+    next(): T | null;
+    hasNext(): boolean;
+    current(): T | null;
+}
+
+class Pokemon {
+    public name: string;
+    public type: string;
+ 
+    constructor(name: string, type: string) {
+        this.name = name;
+        this.type = type;
+    }
+}
+
+class PokemonCollection {
+    private pokemons: Pokemon[] = [];
+
+    public addPokemon(pokemon: Pokemon): void {
+        this.pokemons.push(pokemon);
+    }
+
+    getPokemonAt(index: number): Pokemon | null {
+        if(index >= 0 && index < this.pokemons.length) {
+            return this.pokemons[index];
+        }
+        return null;
+    }
+
+    getLength(): number {
+        return this.pokemons.length;
+    }
+
+    public createIterator(): PokemonIterator {
+       return new PokemonIterator(this);
+    }
+
+}
+
+class PokemonIterator implements Iterator<Pokemon> {
+    private collection: PokemonCollection;
+    private position: number;
+
+    constructor(collection: PokemonCollection) {
+        this.collection = collection;
+        this.position = 0;
+    }
+
+  next(): Pokemon | null {
+    if(this.hasNext()) {
+        return this.collection.getPokemonAt(this.position++);
+    }
+    return null;
+  }
+  hasNext(): boolean {
+    return this.position < this.collection.getLength();
+  }
+  current(): Pokemon | null {
+    return this.collection.getPokemonAt(this.position);
+  }
+
+}
+
+function main(){
+
+    const pokedex = new PokemonCollection();
+    pokedex.addPokemon( new Pokemon('Pikachu', 'Electric') );
+    pokedex.addPokemon( new Pokemon('Charmander', 'Fire') );
+    pokedex.addPokemon( new Pokemon('Squirtle', 'Water') );
+    pokedex.addPokemon( new Pokemon('Bulbasaur', 'Grass') );
+    const iterator = pokedex.createIterator();
+
+    while( iterator.hasNext() ) {
+        const pokemon = iterator.next();
+        if(pokemon){
+            console.log( `${ pokemon?.name } - ${ pokemon?.type }` );
+        }
+    }
+}
+
+
+main();
--- 03-comportamiento/04-mediator.ts
+++ 03-comportamiento/04-mediator.ts
@@ -9,3 +9,69 @@
  *
  * https://refactoring.guru/es/design-patterns/mediator
  */
+//chatroom
+
+import { COLORS } from "../helpers/colors.ts";
+
+class ChatRoom {
+    private users: User[] = [];
+    public title: string;
+    
+    constructor(title: string) {
+        this.title = title;
+    }
+
+    addUser(user: User): void {
+        this.users.push(user);
+    }
+
+    sendMessage(message: string, sender: User): void {
+        const usersToSend = this.users.filter(user => user !== sender);
+        for (const user of usersToSend) {
+            user.receiveMessage(sender, message);
+            // if (user !== sender) {
+            //     // user.receiveMessage(message, sender);
+            //     console.log(`Mensaje de ${sender['username']} a ${user['username']}: ${message}`);
+            // }
+        }
+    }
+
+
+}
+
+class User {
+    private username: string;
+    private chatRoom: ChatRoom;
+
+    constructor(username: string, chatRoom: ChatRoom) {
+        this.username = username;
+        this.chatRoom = chatRoom;
+        chatRoom.addUser(this);
+    }
+
+    sendMessage(message: string): void {
+        console.log(`\n\n\n%c${this.username} envía: %c${message}`,COLORS.blue,COLORS.white);
+        this.chatRoom.sendMessage(message, this);
+    }
+
+    receiveMessage(sender: User, message: string): void {
+        console.log(`%c${this.username} recibe mensaje de ${sender['username']}: %c${message}`,COLORS.blue,COLORS.white);
+    }
+
+
+}
+
+function main(): void {
+    const chatRoom = new ChatRoom('Patrones de Diseño');
+    const user1 = new User('Alice', chatRoom);
+    const user2 = new User('Bob', chatRoom);
+    const user3 = new User('Charlie', chatRoom);
+
+    user1.sendMessage('Hola a todos!');
+    user2.sendMessage('¡Hola Alice! ¿Cómo estás?');
+    user3.sendMessage('¡Hola a ambos! ¿Qué están haciendo?');
+
+    console.log('\n');
+}
+
+main();
